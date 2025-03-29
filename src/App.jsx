ece7aa8a87ebc2e235a511ae3e7a202e78185dad
sourovroy999 <sourovroy3050@gmail.com>
2025-03-30 00:06:12 +0600

    add and reduce learn

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './Counter'
 
 function App() {
   
   function handleClick(){
-    alert('handle clicked')
+    alert('Button clicked')
   }
+
+  const handleClick2=()=>{
+    alert('button 2 clicked')
+  }
+
+  const addToFive=(num)=>{
+    alert(num+5)
+  }
+
+  // we can return function-->
+//   function useState(num) {
+//     function updateNum(value) {
+//         num=value;
+//     }return[num, updateNum]
+// }
+// const[count, setCount]=useState(6)
+
 
   return (
 
 
     <>
       <h1>React Core Concepts Part 2</h1>
+
+      <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>click2</button>
+
       
+      <button onClick={()=>alert('Third clicked')}>click3</button>
+      
+      {/* parameter thakle arrow function use korbo */}
+      <button onClick={()=> addToFive(9)}>Four</button>
     </>
   )
 }
